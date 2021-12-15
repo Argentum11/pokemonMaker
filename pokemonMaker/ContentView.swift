d49d8914555aee04983bf2cb6f5ea065170a3a8f
leoChang <leochang@gmail.com>
2021-12-15 16:09:20 +0800

    Initial Commit

---
+++ pokemonMaker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokemonMaker
+//
+//  Created by User16 on 2021/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokemonMaker/pokemonMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonMakerApp.swift
+//  pokemonMaker
+//
+//  Created by User16 on 2021/12/15.
+//
+
+import SwiftUI
+
+@main
+struct pokemonMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
